2e58faa91222bf7bcce8c4fe7ef25271790b6a9f
Evan St Martin <mantisTMD1@gmail.com>
2023-02-22 15:03:46 -0600

    set up weather api url and installed axios

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 import './style.css'
+import {getWeather} from './weather.js';
 
+getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone).then(res =>
+ {
+    console.log(res.data)
+ }   )
